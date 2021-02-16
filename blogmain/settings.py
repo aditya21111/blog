40207979e1def01fbf27552fa2deb29d119bf188
aditya <extracorrection@gmail.com>
2021-02-16 02:59:17 -0800

    error

--- blogmain/settings.py
+++ blogmain/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'blogmain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
