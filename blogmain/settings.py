9489b38904a6498fe4598449ab004fb4a75c034b
aditya <extracorrection@gmail.com>
2021-02-15 23:16:32 -0800

    allowed host

--- blogmain/settings.py
+++ blogmain/settings.py
@@ -134,9 +134,15 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 STACTIC_ROOT= os.path.join(BASE_DIR,'static')
-STATICFILES_STORAGE="whitenoise.storage.compressesManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MESSAGE_TAGS = {
     messages.ERROR:"danger"
 }
 # Activate Django-Heroku
 django_heroku.settings(locals())
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
