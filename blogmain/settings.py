637be8aa801b1453261685937c41a5b1b71652bc
aditya <extracorrection@gmail.com>
2021-02-15 23:54:08 -0800

    allowed host

--- blogmain/settings.py
+++ blogmain/settings.py
@@ -17,8 +17,7 @@ import os
 import django_heroku
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,13 +85,13 @@ WSGI_APPLICATION = 'blogmain.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default' {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+db_from_env = dj_database_url.config()
+DATABASES["default"].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
