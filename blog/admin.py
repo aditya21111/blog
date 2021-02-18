6d978f03f5bf06f7917dfb9281c5f6a935b8917b
aditya <extracorrection@gmail.com>
2021-02-17 23:35:50 -0800

    rich text editor initilised

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Blogpost,comments
-admin.site.register(Blogpost,comments)
+admin.site.register((Blogpost,comments))
 
-class PostAdmin(admin.ModelAdmin):
-    class Media:
-        js=('tinymceinject.js')
