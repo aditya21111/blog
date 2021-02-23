e955f89d9579e1a4d3c7f153c9fefdd4c576bf8c
aditya <extracorrection@gmail.com>
2021-02-23 08:42:17 -0800

    styling buttons

--- home/urls.py
+++ home/urls.py
@@ -13,11 +13,7 @@ urlpatterns = [
     path("logout/",views.logout,name="logout"),
     path('accounts/', include('allauth.urls')),
     re_path(r'^oauth/', include('social_django.urls', namespace='social')), 
-    re_path(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    re_path(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    auth_views.password_reset_confirm, name='password_reset_confirm'),
-    re_path(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'), # <--
+    re_path('^', include('django.contrib.auth.urls')),# <--
     
     path("profile/",views.profile,name="profile")
     
